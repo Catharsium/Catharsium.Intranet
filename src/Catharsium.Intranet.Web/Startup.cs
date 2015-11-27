330d9ca27f59db74b84f4597438371219ad05732
Catharsium <t.w.brachthuizer@gmail.com>
2015-11-27 21:20:30 +0100

    Project setup

---
+++ src/Catharsium.Intranet.Web/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+
+namespace Catharsium.Intranet.Web
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseIISPlatformHandler();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
